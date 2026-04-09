49dd7423dc3e519fc69953dbd06256a91927861e
Toku1999 <151664279+Toku1999@users.noreply.github.com>
2026-04-09 22:04:53 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('CI/CD App Running 🚀');
+});
+
+// Health check (important for DevOps)
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'UP' });
+});
+
+// Sample API
+app.get('/api/users', (req, res) => {
+    res.json([
+        { id: 1, name: "Tokesh" },
+        { id: 2, name: "DevOps Engineer" }
+    ]);
+});
+
+const PORT = 3000;
+
+// Start server
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
