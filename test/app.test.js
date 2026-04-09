8ff112fb6588131e8f99f64283465a314b14ef58
Toku1999 <151664279+Toku1999@users.noreply.github.com>
2026-04-09 22:33:55 +0530

    Rename test for GET / endpoint to welcome message

--- test/app.test.js
+++ test/app.test.js
@@ -3,7 +3,7 @@ const app = require('../app');
 
 describe('App Tests', () => {
 
-    test('GET / should return success message', async () => {
+    test('GET / should return welcome message', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
         expect(res.text).toBe('CI/CD App Running 🚀');
